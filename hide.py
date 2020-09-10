0ee168b9fbde4b8a4829763ab2a6f889cd1e26b6
Javier <javiermoralesr95@gmail.com>
2020-09-10 16:15:22 -0300

    Added argument parser for hide.py

--- hide.py
+++ hide.py
@@ -1,3 +1,4 @@
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import pai_io
@@ -21,7 +22,6 @@ def match_images_dimensions(image_A, image_B):
     return image_A, image_B
 
 def set_bits_hidden_value(image, bits_hidden):
-    bits_hidden = np.uint8(bits_hidden)
     if len(output.shape) == 2:
         output[0][0] = bits_hidden
     else:
@@ -29,19 +29,16 @@ def set_bits_hidden_value(image, bits_hidden):
     return output
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        raise SyntaxError('Correct usage -> hide.py [path to image A] [path to image B] [number of bits to hide]')
-    try:
-        int(sys.argv[3])
-    except:
-        raise ValueError('Number of bits must be an integer between 0 and 8')
-    if int(sys.argv[3]) > 8:
-        raise ValueError('Number of bits to hide cannot be higher than 8')
-    image_A = pai_io.imread(sys.argv[1])
-    image_B = pai_io.imread(sys.argv[2])
+    parser = argparse.ArgumentParser(description='Hide an image inside another.')
+    parser.add_argument('-imageA', type=str, help='Image that hides another', required=True)
+    parser.add_argument('-imageB', type=str, help='Image that is hidden', required=True)
+    parser.add_argument('-b', type=np.uint8, help='Amount of bits to hide', required=True)
+    args = parser.parse_args()
+    image_A = pai_io.imread(args.imageA)
+    image_B = pai_io.imread(args.imageB)
     image_A, image_B = match_images_dimensions(image_A, image_B)
-    output = hide_image(image_A, image_B, int(sys.argv[3]))
-    output = set_bits_hidden_value(output, int(sys.argv[3]))
+    output = hide_image(image_A, image_B, args.b)
+    output = set_bits_hidden_value(output, args.b)
     plt.imshow(output)
     plt.axis('off')
     plt.show()
