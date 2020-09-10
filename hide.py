b86289842c207acc80fd16e2a9c7acef32dbcde7
Javier <javiermoralesr95@gmail.com>
2020-09-10 15:31:57 -0300

    Now result image is always the size of image A.

--- hide.py
+++ hide.py
@@ -9,14 +9,15 @@ def hide_image(image_A, image_B, bits_len):
     return image_A | image_B
 
 def match_images_dimensions(image_A, image_B):
-    min_y = min(len(image_A), len(image_B))
-    min_x = min(len(image_A[0]), len(image_B[0]))
-    image_A = image_A[:min_y, :min_x]
-    image_B = image_B[:min_y, :min_x]
     if len(image_A.shape) > len(image_B.shape):
         image_B = np.stack((image_B,)*image_A.shape[2], axis=-1)
     if len(image_B.shape) > len(image_A.shape):
         image_A = np.stack((image_A,)*image_B.shape[2], axis=-1)
+    template = np.zeros(image_A.shape, dtype=np.uint8)
+    min_y = min(image_A.shape[0], image_B.shape[0])
+    min_x = min(image_A.shape[1], image_B.shape[1])
+    template[:min_y, :min_x] = image_B[:min_y, :min_x]
+    image_B = template
     return image_A, image_B
 
 def set_bits_hidden_value(image, bits_hidden):
@@ -36,8 +37,8 @@ if __name__ == '__main__':
         raise ValueError('Number of bits must be an integer between 0 and 8')
     if int(sys.argv[3]) > 8:
         raise ValueError('Number of bits to hide cannot be higher than 8')
-    image_A = pai_io.imread(sys.argv[1], as_gray=True)
-    image_B = pai_io.imread(sys.argv[2], as_gray=True)
+    image_A = pai_io.imread(sys.argv[1])
+    image_B = pai_io.imread(sys.argv[2])
     image_A, image_B = match_images_dimensions(image_A, image_B)
     output = hide_image(image_A, image_B, int(sys.argv[3]))
     output = set_bits_hidden_value(output, int(sys.argv[3]))
